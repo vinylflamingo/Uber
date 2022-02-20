86b4f81ecd2cca5a1e1c0164203f113ad227601e
Frank Costoya <66138552+vinylflamingo@users.noreply.github.com>
2022-02-19 23:58:06 -0500

    1

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider} from "react-redux";
+import HomeScreen from './screens/HomeScreen';
+import { store } from "./store";
+// 1) Setup Redux
+//
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <HomeScreen />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //flex: 1,
+    //backgroundColor: '#000',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=bvn_HYpix6s
+
+bookmark: 55:41
